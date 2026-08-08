327783b5bc659f167bf84bc9bf497a78bc945b6e
jrgallop-code <jrgallop@gmail.com>
2026-08-08 12:03:37 -0300

    Include water and sleep in Drive transfers

--- js/core/google-drive-sync.js
+++ js/core/google-drive-sync.js
@@ -22,7 +22,9 @@ const BACKUP_KEYS = [
     "forge_workout_sessions",
     "forge_custom_exercises",
     "forge_weight_entries",
-    "level_up_nutrition_habits"
+    "level_up_nutrition_habits",
+    "level_up_water_entries",
+    "level_up_sleep_entries"
 ];
 
 
