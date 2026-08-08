3e5c4a2aaf6fa1ac451939c3fc20ab9fd9f59963
jrgallop-code <jrgallop@gmail.com>
2026-08-08 18:52:27 -0300

    Refresh goal projection target linkage

--- js/core/router.js
+++ js/core/router.js
@@ -81,7 +81,7 @@ import {
     renderGoalProjection,
     initializeGoalProjection
 }
-from "../nutrition/goal-projection.js?v=active-target-2";
+from "../nutrition/goal-projection.js?v=selected-target-projection-1";
 
 import {
     renderMore,
