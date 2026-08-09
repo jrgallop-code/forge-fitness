6951683bfad43a0f81793e839dd37f04381964b0
jrgallop-code <jrgallop@gmail.com>
2026-08-09 20:54:41 -0300

    Refresh all Water route imports

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-import { navigate } from "./core/router.js?v=router-water-only-1";
+import { navigate } from "./core/router.js?v=router-water-only-2";
 import { renderNavbar, initializeNavbar } from "./components/navbar.js?v=navbar-icons-3";
 import { initializeWorkoutRuntime } from "./workouts/workout-session.js?v=workout-session-4";
 
