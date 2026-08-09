9c46a3a83a14dbe10fdaca3eb20a4d1addb17ea6
jrgallop-code <jrgallop@gmail.com>
2026-08-09 20:46:21 -0300

    Refresh generated measurement router

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-import { navigate } from "./core/router.js?v=router-measurements-silhouette-2";
+import { navigate } from "./core/router.js?v=router-measurements-image-1";
 import { renderNavbar, initializeNavbar } from "./components/navbar.js?v=navbar-icons-3";
 import { initializeWorkoutRuntime } from "./workouts/workout-session.js?v=workout-session-4";
 
