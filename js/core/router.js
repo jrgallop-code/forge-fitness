d9002009615a7b9af185cd0ac9d1b83e61c4d853
jrgallop-code <jrgallop@gmail.com>
2026-08-09 01:22:47 -0300

    Load corrected nutrition module

--- js/core/router.js
+++ js/core/router.js
@@ -44,7 +44,7 @@ import {
     initializeNutrition,
     showNutritionView
 }
-from "../nutrition/nutrition-ui.js?v=nutrition-more-3";
+from "../nutrition/nutrition-ui.js?v=nutrition-more-4";
 
 import {
     renderEnergyProfile,
