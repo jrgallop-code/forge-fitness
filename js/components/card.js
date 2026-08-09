51a6fcd0744b643e3b5ce76251582ac547f8e67c
jrgallop-code <jrgallop@gmail.com>
2026-08-08 23:43:03 -0300

    Use consistent dumbbell and exercise lifter icons

--- js/components/card.js
+++ js/components/card.js
@@ -1,18 +1,25 @@
+const DUMBBELL_ICON = `
+    <svg class="app-silhouette-icon" viewBox="0 0 24 24" aria-hidden="true">
+        <path d="M2.7 10.2h2v3.6h-2v-3.6Zm3-2h2.1v7.6H5.7V8.2Zm2.9 2.8h6.8v2H8.6v-2Zm7.6-2.8h2.1v7.6h-2.1V8.2Zm3.1 2h2v3.6h-2v-3.6Z"/>
+    </svg>
+`;
+
+const EXERCISE_LIFTER_ICON = `
+    <svg class="app-silhouette-icon app-lifter-icon" viewBox="0 0 24 24" aria-hidden="true">
+        <circle cx="12" cy="6" r="2.1"/>
+        <path d="M10.3 9h3.4l1.1 4.1 2.5 2.2-1.4 1.6-3-2.6-.9-2.2-.9 2.2-3 2.6-1.4-1.6 2.5-2.2L10.3 9Z"/>
+        <path d="M9.9 15.2 8.1 21h2.3l1.6-3.8 1.6 3.8h2.3l-1.8-5.8H9.9Z"/>
+        <path d="M3.2 3.3h2v5h-2v-5Zm2.8 1.2h2v2.6H6V4.5Zm2.2.6h7.6v1.4H8.2V5.1Zm7.8-.6h2v2.6h-2V4.5Zm2.8-1.2h2v5h-2v-5Z"/>
+    </svg>
+`;
+
 const ICONS = {
-    "🏋️": `
-        <svg class="app-silhouette-icon" viewBox="0 0 24 24" aria-hidden="true">
-            <path d="M2.7 10.2h2v3.6h-2v-3.6Zm3-2h2.1v7.6H5.7V8.2Zm2.9 2.8h6.8v2H8.6v-2Zm7.6-2.8h2.1v7.6h-2.1V8.2Zm3.1 2h2v3.6h-2v-3.6Z"/>
-        </svg>
-    `,
+    "🏋️": DUMBBELL_ICON,
+    "💪": DUMBBELL_ICON,
+    "exercise": EXERCISE_LIFTER_ICON,
     "✓": `
         <svg class="app-silhouette-icon app-silhouette-stroke" viewBox="0 0 24 24" aria-hidden="true">
             <path d="m4.5 12.4 4.4 4.4L19.7 6.4"/>
-        </svg>
-    `,
-    "💪": `
-        <svg class="app-silhouette-icon bicep-silhouette-icon" viewBox="0 0 24 24" aria-hidden="true">
-            <path d="M7.2 3.1c.8-.5 1.8-.2 2.3.5l.8 1.2 1.1-.8c.7-.5 1.7-.3 2.2.4.4.6.3 1.5-.2 2l-2.1 1.8v3.2l1.1-1c1.2-1.1 2.8-1.7 4.4-1.7 2.9 0 5.3 2.1 5.7 4.9.5 3.6-2.3 6.9-6 6.9H8.1c-3.1 0-5.6-2.5-5.6-5.6 0-1.7.8-3.4 2.1-4.4l2.3-1.8V5.1c0-.8.1-1.5.3-2Z"/>
-            <path d="M7.1 2.8 9 2.1l1.2 2-2.1.9-1-2.2Zm3.3 2 1.8-1.3 1.2 1.7-2 1.5-1-1.9Z"/>
         </svg>
     `,
     "📋": `
@@ -40,7 +47,11 @@ const ICONS = {
 
 export function createCard(title, value, icon){
 
-const renderedIcon = ICONS[icon] || icon;
+const resolvedIcon = /exercise/i.test(String(title))
+    ? "exercise"
+    : icon;
+
+const renderedIcon = ICONS[resolvedIcon] || resolvedIcon;
 
 return `
 
