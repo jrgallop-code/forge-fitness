c55fbfb3920ec588eaf1f3e5158633a83ddd06ad
jrgallop-code <jrgallop@gmail.com>
2026-08-10 16:29:26 -0300

    Make the plan-builder form guide action clearly visible
    * Make plan-builder guide action clearly visible
    
    * Emphasize the compact builder guide button
    
    * Bump visible builder guide module cache
    
    * Use router with visible builder guides
    
    * Bump router and navbar caches for visible guides
    
    * Bump visible builder guide asset caches

--- js/components/navbar.js
+++ js/components/navbar.js
@@ -1,4 +1,4 @@
-import { navigate } from "../core/router.js?v=builder-form-guide-links-1";
+import { navigate } from "../core/router.js?v=visible-builder-guide-1";
 
 
 export function renderNavbar() {
