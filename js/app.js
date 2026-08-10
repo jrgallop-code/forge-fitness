2517a719cce1990a3de5d36ad2b0fdcf816ffd27
jrgallop-code <jrgallop@gmail.com>
2026-08-09 22:40:24 -0300

    Bump native catalogue router cache

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-import { navigate } from "./core/router.js?v=router-workout-catalogue-fix-1";
+import { navigate } from "./core/router.js?v=router-native-catalogue-1";
 import { renderNavbar, initializeNavbar } from "./components/navbar.js?v=navbar-water-route-1";
 import { initializeWorkoutRuntime } from "./workouts/workout-session.js?v=workout-session-4";
 
