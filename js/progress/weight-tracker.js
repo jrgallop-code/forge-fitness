65da81497340b761f8347a35d39a1780930354e7
jrgallop-code <jrgallop@gmail.com>
2026-08-10 07:28:54 -0300

    Notify recommendation modules when weight data changes

--- js/progress/weight-tracker.js
+++ js/progress/weight-tracker.js
@@ -259,6 +259,14 @@ function saveWeightEntries(entries) {
         WEIGHT_STORAGE_KEY,
         JSON.stringify(entries)
     );
+
+    window.dispatchEvent(
+        new CustomEvent("levelup:weight-updated", {
+            detail: {
+                entryCount: Array.isArray(entries) ? entries.length : 0
+            }
+        })
+    );
 }
 
 
