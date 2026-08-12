4fecc19069e151cd6c5619ea6bae472bf24944b8
jrgallop-code <jrgallop@gmail.com>
2026-08-12 14:26:45 -0300

    Fix Add Exercise freeze in manual builder (#182)
    * Remove mutation observer from manual superset builder
    
    * Bump manual superset builder cache key

--- js/core/router.js
+++ js/core/router.js
@@ -1,6 +1,6 @@
 import { renderWorkoutBuilder } from "../workouts/workout-ui.js?v=plan-builder-library-refresh-1";
 import { initializeWorkoutBuilder } from "../workouts/workouts.js?v=builder-library-refresh-1";
-import { initializeManualSupersetBuilder } from "../workouts/manual-superset-builder.js?v=manual-supersets-2";
+import { initializeManualSupersetBuilder } from "../workouts/manual-superset-builder.js?v=manual-supersets-3";
 import { initializeOneOffWorkout } from "../workouts/one-off-workout.js?v=one-off-workout-1";
 import { initializeWorkoutCatalogue } from "../workouts/workout-catalogue.js?v=workout-catalogue-form-coach-removed-1";
 import { initializeSmartBuild } from "../workouts/smart-build.js?v=smart-build-2";
