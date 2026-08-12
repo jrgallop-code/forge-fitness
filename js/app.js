e204ab7ddb927ec005a799068cec4dafbf272320
jrgallop-code <jrgallop@gmail.com>
2026-08-11 22:44:14 -0300

    Bust router cache for Progress weight initialization

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-import { navigate } from "./core/router.js?v=progress-summary-route-1";
+import { navigate } from "./core/router.js?v=progress-weight-instant-2";
 import { renderNavbar, initializeNavbar } from "./components/navbar.js?v=cardio-fields-1";
 import { initializeWorkoutRuntime } from "./workouts/workout-session.js?v=workout-session-7-cardio-fields";
 
