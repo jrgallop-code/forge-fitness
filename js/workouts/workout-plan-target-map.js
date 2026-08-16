e533f87b1d579108639cda9f9db4d09284e87016
jrgallop-code <jrgallop@gmail.com>
2026-08-16 18:38:45 -0300

    Keep target map diff mapping-only

--- js/workouts/workout-plan-target-map.js
+++ js/workouts/workout-plan-target-map.js
@@ -350,7 +350,7 @@ function escapeHtml(value) {
         .replace(/&/g, "&amp;")
         .replace(/</g, "&lt;")
         .replace(/>/g, "&gt;")
-        .replace(/"/g, "&quot;")
+        .replace(/\\\"/g, "&quot;")
         .replace(/'/g, "&#039;");
 }
 
