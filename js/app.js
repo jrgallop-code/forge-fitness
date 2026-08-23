433dd454de22ef696bfcce92a789a44f211a718c
jrgallop-code <jrgallop@gmail.com>
2026-08-23 10:53:29 -0300

    Fix workout summary scrolling (js/app.js)

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-import { navigate } from "./core/router.js?v=drop-sets-8";
+import { navigate } from "./core/router.js?v=summary-scroll-1";
 import { renderNavbar, initializeNavbar } from "./components/navbar.js?v=beta-account-4";
 import { initializeWorkoutRuntime } from "./workouts/workout-session.js?v=drop-sets-6";
 import { scheduleIconDecoration } from "./core/icon-decoration-scheduler.js?v=icon-scheduler-1";
