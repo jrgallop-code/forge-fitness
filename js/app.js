42e3e7c5c77a8783003c532919705962fefb4ecc
jrgallop-code <jrgallop@gmail.com>
2026-08-28 10:58:57 -0300

    Refresh inline meal editor assets

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-import { navigate } from "./core/router.js?v=meal-item-editor-overlay-1";
+import { navigate } from "./core/router.js?v=inline-meal-item-editor-1";
 import { renderNavbar, initializeNavbar } from "./components/navbar.js?v=deload-workout-preview-1";
 import { initializeWorkoutRuntime } from "./workouts/workout-session.js?v=repair-generic-exercise-1";
 import { scheduleIconDecoration } from "./core/icon-decoration-scheduler.js?v=icon-scheduler-1";
