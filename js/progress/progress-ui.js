04cedc34dba152aee1ed675ecddd04471d6478cc
jrgallop-code <jrgallop@gmail.com>
2026-08-30 08:21:04 -0300

    Stabilize weekly TDEE: js/progress/progress-ui.js

--- js/progress/progress-ui.js
+++ js/progress/progress-ui.js
@@ -1,4 +1,4 @@
-import { renderCalorieStats } from "../nutrition/calorie-stats.js?v=tdee-food-window-1";
+import { renderCalorieStats } from "../nutrition/calorie-stats.js?v=weekly-stable-tdee-1";
 
 export function renderProgress() {
 
