00ebfb303b5e1838f9a2a45e43415620d6678504
jrgallop-code <jrgallop@gmail.com>
2026-08-31 19:45:12 -0300

    Use form guide SVGs in exercise carousel

--- js/workouts/manual-builder-catalogue.js
+++ js/workouts/manual-builder-catalogue.js
@@ -1,5 +1,5 @@
 import { getAllExercises, getExerciseById } from "./exercise-library.js?v=exercise-library-catalogue-2";
-import { matchesExerciseBrowser, renderMuscleCarousel } from "./exercise-browser.js?v=static-anatomy-assets-1";
+import { matchesExerciseBrowser, renderMuscleCarousel } from "./exercise-browser.js?v=form-guide-carousel-1";
 
 let active = false;
 let picker = null;
