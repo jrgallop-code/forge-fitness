058c22ba85b4466633b92b6daf06f19c8f87cd12
jrgallop-code <jrgallop@gmail.com>
2026-08-31 15:17:14 -0300

    Wire Weight and Carbs detail toggle

--- js/progress/weight-carbs-chart.js
+++ js/progress/weight-carbs-chart.js
@@ -1,5 +1,7 @@
 import { initializeWeightCarbsChartV2 } from "./weight-chart-carousel-v2.js?v=weight-carousel-authoritative-1";
+import { initializeWeightCarbsDetailToggle } from "./weight-carbs-detail-toggle.js?v=weight-carbs-detail-toggle-1";
 
 export function initializeWeightCarbsChart(root = document) {
     initializeWeightCarbsChartV2(root);
+    initializeWeightCarbsDetailToggle(root);
 }
