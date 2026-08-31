38db9ee2865b17a01e502f89526002554a36d056
jrgallop-code <jrgallop@gmail.com>
2026-08-30 22:49:04 -0300

    Refresh weight summary module cache chain

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-import { navigate } from "./core/router.js?v=shared-live-trend-1";
+import { navigate } from "./core/router.js?v=weight-period-summary-2";
 import { renderNavbar, initializeNavbar } from "./components/navbar.js?v=nutrition-nav-1";
 import { initializeWorkoutRuntime } from "./workouts/workout-session.js?v=cardio-rpe-1";
 import { scheduleIconDecoration } from "./core/icon-decoration-scheduler.js?v=icon-scheduler-1";
