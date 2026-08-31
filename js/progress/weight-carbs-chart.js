1b386b18ea4126d37de008a576eb6ebf8dbcc3f7
jrgallop-code <jrgallop@gmail.com>
2026-08-31 17:01:26 -0300

    Wire weight point and sodium context

--- js/progress/weight-carbs-chart.js
+++ js/progress/weight-carbs-chart.js
@@ -1,7 +1,9 @@
 import { initializeWeightCarbsChartV2 } from "./weight-chart-carousel-v2.js?v=weight-carousel-authoritative-1";
 import { initializeWeightCarbsInteractionEnhancements } from "./weight-carbs-interaction-enhancements.js?v=weight-carbs-interaction-2";
+import { initializeWeightSecondaryContext } from "./weight-secondary-context.js?v=weight-secondary-context-1";
 
 export function initializeWeightCarbsChart(root = document) {
     initializeWeightCarbsChartV2(root);
     initializeWeightCarbsInteractionEnhancements(root);
+    initializeWeightSecondaryContext(root);
 }
