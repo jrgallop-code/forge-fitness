75ab808f7eca64315405e698afc361604e3fb7cc
jrgallop-code <jrgallop@gmail.com>
2026-08-31 16:21:01 -0300

    Wire Weight and Carbs axis and double-tap enhancements

--- js/progress/weight-carbs-chart.js
+++ js/progress/weight-carbs-chart.js
@@ -1,5 +1,7 @@
 import { initializeWeightCarbsChartV2 } from "./weight-chart-carousel-v2.js?v=weight-carousel-authoritative-1";
+import { initializeWeightCarbsInteractionEnhancements } from "./weight-carbs-interaction-enhancements.js?v=weight-carbs-interaction-1";
 
 export function initializeWeightCarbsChart(root = document) {
     initializeWeightCarbsChartV2(root);
+    initializeWeightCarbsInteractionEnhancements(root);
 }
