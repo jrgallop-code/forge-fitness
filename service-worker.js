651b44446ea73c85e1ee23e53414aafd39560326
jrgallop-code <jrgallop@gmail.com>
2026-08-31 12:24:16 -0300

    Refresh the PWA for the review indicator

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "2026-08-31-53";
+const CACHE_VERSION = "2026-08-31-54";
 const CACHE_PREFIX = "level-up-";
 const SHELL_CACHE = `${CACHE_PREFIX}shell-${CACHE_VERSION}`;
 const RUNTIME_CACHE = `${CACHE_PREFIX}runtime-${CACHE_VERSION}`;
