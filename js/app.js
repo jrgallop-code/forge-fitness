5bbced7c923f4728b9daafaa543c72f675823fb1
jrgallop-code <jrgallop@gmail.com>
2026-08-31 21:46:42 -0300

    Add coach program build transition

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-import { navigate } from "./core/router.js?v=explore-research-2";
+import { navigate } from "./core/router.js?v=coach-build-loading-1";
 import { renderNavbar, initializeNavbar } from "./components/navbar.js?v=progress-nav-stability-1";
 import { initializeWorkoutRuntime } from "./workouts/workout-session.js?v=workout-source-stats-1";
 import { scheduleIconDecoration } from "./core/icon-decoration-scheduler.js?v=icon-scheduler-1";
