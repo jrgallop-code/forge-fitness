8434029df4cff437690143070f1127a64b825b6a
jrgallop-code <jrgallop@gmail.com>
2026-09-03 10:43:58 -0300

    Use shared smoothed context charts

--- js/progress/weight-carbs-chart.js
+++ js/progress/weight-carbs-chart.js
@@ -1,9 +1,9 @@
-import { initializeWeightCarbsChartV2 } from "./weight-chart-carousel-v2.js?v=arctic-contrast-1";
+import { initializeWeightCarbsChartV3 } from "./weight-chart-carousel-v3.js?v=smoothed-visible-trend-1";
 import { initializeWeightCarbsInteractionEnhancements } from "./weight-carbs-interaction-enhancements.js?v=weight-carbs-interaction-2";
-import { initializeWeightCalorieContext } from "./weight-calorie-context.js?v=arctic-surfaces-1";
+import { initializeWeightCalorieContextV2 } from "./weight-calorie-context-v2.js?v=smoothed-visible-trend-1";
 
 export function initializeWeightCarbsChart(root = document) {
-    initializeWeightCarbsChartV2(root);
+    initializeWeightCarbsChartV3(root);
     initializeWeightCarbsInteractionEnhancements(root);
-    initializeWeightCalorieContext(root);
+    initializeWeightCalorieContextV2(root);
 }
