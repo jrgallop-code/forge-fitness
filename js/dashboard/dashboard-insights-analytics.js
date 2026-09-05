a340d36d72cb1c45a29ad822c664e7bfcbdb74b5
jrgallop-code <jrgallop@gmail.com>
2026-09-05 12:57:01 -0300

    Cache-bust Dashboard spacing adjustment

--- js/dashboard/dashboard-insights-analytics.js
+++ js/dashboard/dashboard-insights-analytics.js
@@ -1,3 +1,3 @@
-import "./dashboard-see-more-position-fix.js?v=dashboard-see-more-position-1";
+import "./dashboard-see-more-position-fix.js?v=dashboard-see-more-position-2";
 import "../core/workout-theme-guardrail.js?v=workout-theme-guardrail-3";
 export * from "./dashboard-insights-analytics-v5.js?v=dashboard-insights-5";
