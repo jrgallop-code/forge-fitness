9b041e46672d9a8d96b0e0c9ad89d3c111dd448e
jrgallop-code <jrgallop@gmail.com>
2026-09-05 10:56:43 -0300

    Bust Dashboard analytics cache

--- js/dashboard/dashboard-weight-trend-card.js
+++ js/dashboard/dashboard-weight-trend-card.js
@@ -1,4 +1,4 @@
-import "./dashboard-insights-analytics.js?v=dashboard-insights-1";
+import "./dashboard-insights-analytics.js?v=dashboard-insights-2";
 import { buildDashboardWeightTrendSvg } from "./dashboard-weight-trend-svg.js?v=dashboard-weight-animated-1";
 import { calculateVisibleWeightTrend, normalizeWeightEntries } from "../core/weight-trend.js?v=smoothed-visible-trend-1";
 
