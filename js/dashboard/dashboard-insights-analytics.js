89cda0772f7dccaeaf9ccd079736d8f9a941513b
jrgallop-code <jrgallop@gmail.com>
2026-09-05 11:58:49 -0300

    Load dashboard position and workout theme guardrails

--- js/dashboard/dashboard-insights-analytics.js
+++ js/dashboard/dashboard-insights-analytics.js
@@ -1,1 +1,3 @@
+import "./dashboard-see-more-position-fix.js?v=dashboard-see-more-position-1";
+import "../core/workout-theme-guardrail.js?v=workout-theme-guardrail-1";
 export * from "./dashboard-insights-analytics-v5.js?v=dashboard-insights-5";
