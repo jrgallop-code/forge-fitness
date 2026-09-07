5bceab68bfdffb2697171a6f6ab4b70e306d2af7
jrgallop-code <jrgallop@gmail.com>
2026-09-07 08:37:46 -0300

    Load repeat workout day controls

--- js/progress/training-progress-collapse.js
+++ js/progress/training-progress-collapse.js
@@ -1,6 +1,7 @@
 import "./appearance-volume-goals-fix.js?v=appearance-volume-goals-4";
 import "./weight-viewport-summary-authority.js?v=weight-viewport-summary-2";
 import "../workouts/workout-delete-message-cleanup.js?v=workout-delete-message-cleanup-1";
+import "../workouts/repeat-workout-day.js?v=repeat-workout-day-1";
 
 function unwrapLegacyDisclosure(liftingProgress) {
     const existing = liftingProgress.querySelector(":scope > .training-progress-disclosure");
