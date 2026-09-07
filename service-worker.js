22c97f7072eb8a289ea68d258a5dbfdbbf396629
jrgallop-code <jrgallop@gmail.com>
2026-09-07 18:16:28 -0300

    Refresh PWA cache for warm-up controls

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "2026-09-07-264";
+const CACHE_VERSION = "2026-09-07-265";
 const CACHE_PREFIX = "level-up-";
 const SHELL_CACHE = `${CACHE_PREFIX}shell-${CACHE_VERSION}`;
 const RUNTIME_CACHE = `${CACHE_PREFIX}runtime-${CACHE_VERSION}`;
