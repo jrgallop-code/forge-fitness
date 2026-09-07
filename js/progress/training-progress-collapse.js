635f2476ab95ebfb33249096542e985a2d362a83
jrgallop-code <jrgallop@gmail.com>
2026-09-06 23:51:20 -0300

    Load optional dynamic warm-up experience

--- js/progress/training-progress-collapse.js
+++ js/progress/training-progress-collapse.js
@@ -1,4 +1,5 @@
 import "./appearance-volume-goals-fix.js?v=appearance-volume-goals-4";
+import "../workouts/dynamic-warmup.js?v=dynamic-warmup-1";
 
 function unwrapLegacyDisclosure(liftingProgress) {
     const existing = liftingProgress.querySelector(":scope > .training-progress-disclosure");
