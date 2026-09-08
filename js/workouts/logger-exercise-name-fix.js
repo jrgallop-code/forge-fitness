1b0fe91f5f740d5d630de7cbfcf1009bb96bd2f6
jrgallop-code <jrgallop@gmail.com>
2026-09-07 21:05:26 -0300

    Remove duplicate starting weight launcher

--- js/workouts/logger-exercise-name-fix.js
+++ js/workouts/logger-exercise-name-fix.js
@@ -1,5 +1,4 @@
 import "./plate-calculator.js?v=progressive-plate-sizes-1";
-import "./starting-weight-calibration.js?v=granular-units-1";
 
 const ACTIVE_WORKOUT_STORAGE_KEY = "level_up_active_workout";
 
