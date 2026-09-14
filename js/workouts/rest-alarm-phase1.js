535ce0b434eb436c5092450b0c9eac9a8a331d21
jrgallop-code <jrgallop@gmail.com>
2026-09-14 07:17:38 -0300

    Cancel rest when warm-up is unchecked

--- js/workouts/rest-alarm-phase1.js
+++ js/workouts/rest-alarm-phase1.js
@@ -1,7 +1,7 @@
 import { openActiveWorkout, ACTIVE_WORKOUT_STORAGE_KEY } from "./workout-session.js?v=native-navigation-stability-1";
 import { getExerciseById } from "./exercise-library.js?v=exercise-library-3";
 import { requestNativeAlarmPermission, scheduleNativeAlarm } from "../core/native-capabilities.js?v=lock-screen-timers-2";
-import { cancelActiveRestTimer } from "./rest-timer-authority.js?v=cancel-running-timer-1";
+import { cancelActiveRestTimer } from "./rest-timer-authority.js?v=warmup-toggle-cancel-1";
 
 const EXERCISE_TIMER_SETTINGS_KEY = "level_up_exercise_rest_settings";
 const ALARM_PREFS_KEY = "level_up_rest_alarm_preferences";
