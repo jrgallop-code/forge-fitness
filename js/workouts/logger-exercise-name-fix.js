e13b3e3a1d74027f313bbbe490272c5cb9a64f68
jrgallop-code <jrgallop@gmail.com>
2026-09-14 13:03:41 -0300

    Fix plate calculator theme leakage

--- js/workouts/logger-exercise-name-fix.js
+++ js/workouts/logger-exercise-name-fix.js
@@ -1,4 +1,4 @@
-import "./plate-calculator.js?v=refined-equipment-controls-3";
+import "./plate-calculator.js?v=refined-equipment-controls-4";
 
 const ACTIVE_WORKOUT_STORAGE_KEY = "level_up_active_workout";
 
