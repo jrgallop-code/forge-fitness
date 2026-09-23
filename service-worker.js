a90056a0adbeb7674f3db7e5d53dae91892ed217
jrgallop-code <jrgallop@gmail.com>
2026-09-23 14:05:41 -0300

    Refresh PWA workout sharing assets

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "2026-09-23-pwa-ios-parity-344";
+const CACHE_VERSION = "2026-09-23-pwa-workout-sharing-345";
 const CACHE_PREFIX = "level-up-";
 const SHELL_CACHE = `${CACHE_PREFIX}shell-${CACHE_VERSION}`;
 const RUNTIME_CACHE = `${CACHE_PREFIX}runtime-${CACHE_VERSION}`;
